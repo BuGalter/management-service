2a97ce6305f140ef494809de9e2441d1190537a0
BuGalter <wardoc78@gmail.com>
2022-01-25 09:49:52 +0300

    faet: Added server on hapi
    Added test routes
    Added connect to db

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,28 @@
 import * as Hapi from '@hapi/hapi';
+import * as Inert from '@hapi/inert';
 import config from './config';
+import sequelize from './models';
+import routes from './routes';
 
 const init = async () => {
   const server = Hapi.server({
     port: config.server.port,
     host: config.server.host,
   });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  }
+  catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+  server.realm.modifiers.route.prefix = '/api';
+
+  await server.register([Inert]);
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server running on port ${config.server.port}`);
