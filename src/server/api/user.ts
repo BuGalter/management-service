c3abe2279454a8189d6ad2aa96d4877c5b2dc668
BuGalter <wardoc78@gmail.com>
2022-01-25 15:40:08 +0300

    fix: Added console.log to created user

--- src/server/api/user.ts
+++ src/server/api/user.ts
@@ -2,12 +2,13 @@ import { User, } from '../models/User';
 import { validatePayload, } from '../utils/validate';
 
 export async function userReg(r) {
-  const payload = r.payload;
+  const { ...payload } = r.payload;
   const data = await validatePayload(payload);
   if (typeof (data) === 'string') {
     return { status: 404, message: data, };
   }
 
   const newUser = await User.create(data);
-  return { status: 200, message: newUser, };
+  console.log(newUser);
+  return { status: 200, message: 'User added!', };
 }
