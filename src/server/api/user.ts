c8bfc2e1810d0ce504a5e06f42ba8036547802f6
BuGalter <wardoc78@gmail.com>
2022-01-25 15:30:15 +0300

    feat: Added heandler for registration route

---
+++ src/server/api/user.ts
@@ -0,0 +1,13 @@
+import { User, } from '../models/User';
+import { validatePayload, } from '../utils/validate';
+
+export async function userReg(r) {
+  const payload = r.payload;
+  const data = await validatePayload(payload);
+  if (typeof (data) === 'string') {
+    return { status: 404, message: data, };
+  }
+
+  const newUser = await User.create(data);
+  return { status: 200, message: newUser, };
+}
