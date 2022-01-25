0bcaa0791154a24f48ded3d98457f5b7a02805a4
BuGalter <wardoc78@gmail.com>
2022-01-25 20:40:59 +0300

    feat: Added token config options

--- src/server/config.ts
+++ src/server/config.ts
@@ -13,4 +13,10 @@ export default {
     port: process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000,
     host: process.env.SERVER_HOST ? process.env.SERVER_HOST : 'localhost',
   },
+  token: {
+    tokenKeyAccess: process.env.TOKEN_KEY_ACCESS,
+    tokenKeyRefresh: process.env.TOKEN_KEY_REFRESH,
+    lifeTimeAccessToken: Number(process.env.LIFE_TIME_ACCESSTOKEN),
+    lifeTimeRefreshToken: process.env.LIFE_TIME_REFRESHTOKEN,
+  },
 };
