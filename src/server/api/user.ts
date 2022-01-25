7ce37efd1464d4cc76ea7856e5ebe59b60889cde
BuGalter <wardoc78@gmail.com>
2022-01-25 20:44:32 +0300

    feat: Added create session to handler for user/auth

--- src/server/api/user.ts
+++ src/server/api/user.ts
@@ -1,9 +1,11 @@
 import { User, } from '../models/User';
-import { validatePayload, } from '../utils/validate';
+import { Session, } from '../models/Session';
+import { validatePayloadReg, validatePayloadAuth, } from '../utils/validate';
+import { createTokensJWT, } from '../utils/token';
 
 export async function userReg(r) {
   const { ...payload } = r.payload;
-  const data = await validatePayload(payload);
+  const data = await validatePayloadReg(payload);
   if (typeof (data) === 'string') {
     return { status: 404, message: data, };
   }
@@ -11,4 +13,24 @@ export async function userReg(r) {
   const newUser = await User.create(data);
   console.log(newUser);
   return { status: 200, message: 'User added!', };
+}
+
+export async function userAuth(r) {
+  const { ...payload } = r.payload;
+  const data = await validatePayloadAuth(payload);
+  if (typeof (data) === 'string') {
+    return { status: 404, message: data, };
+  }
+
+  console.log(data);
+  const user = await User.findOne({
+    where: { name: data.name, },
+  });
+  if (user === null) {
+    return { status: 404, message: 'User not found!', };
+  }
+
+  const newSession = await Session.create({ userId: user.id, });
+  const tokens = await createTokensJWT(newSession.id, user.id);
+  return { status: 200, message: tokens.access, };
 }
