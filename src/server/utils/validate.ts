e88d1ebf7552edadfed63aeacbe181b8b02219d3
BuGalter <wardoc78@gmail.com>
2022-01-25 17:53:09 +0300

    feat: Added validate payload to user authentication

--- src/server/utils/validate.ts
+++ src/server/utils/validate.ts
@@ -1,8 +1,18 @@
-import { schemaUser, } from '../schemes';
+import { schemaUser, schemaAuth, } from '../schemes';
 
-export async function validatePayload(payload) {
+export async function validatePayloadReg(payload) {
   try {
     const value = await schemaUser.validateAsync(payload);
+    return value;
+  }
+  catch (error) {
+    return error.message;
+  }
+}
+
+export async function validatePayloadAuth(payload) {
+  try {
+    const value = await schemaAuth.validateAsync(payload);
     return value;
   }
   catch (error) {
