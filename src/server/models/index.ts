741d8634c662de9c1c655190e0d9dbb7df96f9fa
BuGalter <wardoc78@gmail.com>
2022-01-26 14:40:28 +0300

    Added model session

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -1,11 +1,12 @@
 import { Sequelize, } from 'sequelize-typescript';
 import config from '../config/config';
+import { Session, } from './Session';
 import { User, } from './User';
 
 const sequelize = new Sequelize(config.db.dbName, config.db.userName, config.db.password, {
   host: config.db.dbHost,
   dialect: 'postgres',
-  models: [User],
+  models: [User, Session],
 });
 
 sequelize.sync();
