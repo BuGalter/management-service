8def74262b6a0bf95fe185761e28ce4ef8b0496f
BuGalter <wardoc78@gmail.com>
2022-01-26 18:08:57 +0300

    Change output messages on functions from utils

--- src/server/api/user.ts
+++ src/server/api/user.ts
@@ -1,34 +1,36 @@
 import { User, } from '../models/User';
 import { Session, } from '../models/Session';
 import { validatePayloadReg, validatePayloadAuth, } from '../utils/validate';
-import { createTokensJWT, } from '../utils/token';
+import { generateJwt, } from '../utils/auth';
+import { output, error, } from '../utils';
 
 export async function userReg(r) {
   const { ...payload } = r.payload;
   const data = await validatePayloadReg(payload);
   if (typeof (data) === 'string') {
-    return { status: 404, message: data, };
+    return error(404000, data, null);
   }
 
   const newUser = await User.create(data);
-  return { status: 200, message: `${newUser.name} added!`, };
+  return output({ message: `${newUser.name} added!`, });
 }
 
 export async function userAuth(r) {
   const { ...payload } = r.payload;
   const data = await validatePayloadAuth(payload);
   if (typeof (data) === 'string') {
-    return { status: 404, message: data, };
+    return error(404000, data, null);
   }
 
   const user = await User.findOne({
     where: { name: data.name, },
   });
   if (user === null) {
-    return { status: 404, message: 'User not found!', };
+    return error(404000, 'User not found!', null);
   }
 
   const newSession = await Session.create({ userId: user.id, });
-  const tokens = await createTokensJWT(newSession.id, user.id);
-  return { status: 200, message: tokens.access, };
+  const dataSession = { sessionId: newSession.id, userId: newSession.userId, };
+  const tokens = await generateJwt(dataSession);
+  return output({ message: tokens.access, });
 }
