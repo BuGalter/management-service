e245c2a59794e37e95e3de5b161aba2227c79d1f
BuGalter <wardoc78@gmail.com>
2022-01-26 14:56:05 +0300

    del: Delete test route /user from users

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,18 +1,6 @@
-import { ok, } from 'assert';
-import { User, } from '../models/User';
 import { userAuth, userReg, } from '../api/user';
 
 export default [
-  {
-    method: 'GET',
-    path: '/user',
-    handler: async function (request, h) {
-
-      const users = await User.findAll();
-      console.log(users);
-      return { status: ok, message: users, };
-    },
-  },
   {
     method: 'POST',
     path: '/user/reg',
