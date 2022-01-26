503ac6276a6b17ca3bd58e9d75655fc272d4c63e
BuGalter <wardoc78@gmail.com>
2022-01-26 11:29:33 +0300

    feat: Added schema for auth with jwt token

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,12 +4,23 @@ import * as AuthBearer from 'hapi-auth-bearer-token';
 import config from './config';
 import sequelize from './models';
 import routes from './routes';
+import { validateSession, } from './utils/validate';
 
 const init = async () => {
   const server = Hapi.server({
     port: config.server.port,
     host: config.server.host,
   });
+
+  server.realm.modifiers.route.prefix = '/api';
+
+  await server.register([Inert, AuthBearer]);
+
+  server.auth.strategy('simple', 'bearer-access-token', { validate: validateSession, });
+
+  server.auth.default('simple');
+
+  server.route(routes);
 
   try {
     await sequelize.authenticate();
@@ -18,12 +29,6 @@ const init = async () => {
   catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-
-  server.realm.modifiers.route.prefix = '/api';
-
-  await server.register([Inert, AuthBearer]);
-
-  server.route(routes);
 
   await server.start();
   console.log(`Server running on port ${config.server.port}`);
