691a88548b7347b79327df4a3480046e0422da23
BuGalter <wardoc78@gmail.com>
2022-01-26 14:46:28 +0300

    feat: Added base routes

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,15 +1,18 @@
-import { ok, } from 'assert';
-import { User, } from '../models/User';
-
 export default [
   {
-    method: 'GET',
-    path: '/user',
-    handler: async function (request, h) {
-
-      const users = await User.findAll();
-      console.log(users);
-      return {status : ok, message: users};
+    method: 'POST',
+    path: '/user/reg',
+    options: {
+      auth: false,
     },
+    handler: userReg,
+  },
+  {
+    method: 'POST',
+    path: '/user/auth',
+    options: {
+      auth: false,
+    },
+    handler: userAuth,
   }
 ];
