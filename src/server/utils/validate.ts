d19888226d789b8d11700e1913a3725bf879d6c8
BuGalter <wardoc78@gmail.com>
2022-01-26 11:36:51 +0300

    feat: Added to utils/validate function validateSession for strategy auth jwt

--- src/server/utils/validate.ts
+++ src/server/utils/validate.ts
@@ -1,4 +1,6 @@
 import { schemaUser, schemaAuth, } from '../schemes';
+import { decodeTokenJwt, } from './token';
+import { Session, } from '../models/Session';
 
 export async function validatePayloadReg(payload) {
   try {
@@ -18,4 +20,29 @@ export async function validatePayloadAuth(payload) {
   catch (error) {
     return error.message;
   }
+}
+
+export async function validateSession(request, token, h) {
+  /**
+   * Determine the correctness of the data from the token.
+   * @param {string} token - Token from user request.
+   * @returns {Object<boolean, string, string>}.
+   */
+  let isValid = false;
+  const credentials = { };
+  const artifacts = { };
+  const data = await decodeTokenJwt(token);
+  if (data) {
+    const session = await Session.findOne({
+      where: { id: data.sessionId, userId: data.userId, },
+    });
+    if (session === null) {
+      return { isValid, credentials, artifacts, };
+    }
+
+    isValid = true;
+    return { isValid, credentials, artifacts, };
+  }
+
+  return { isValid, credentials, artifacts, };
 }
