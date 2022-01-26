fb995f23989da4196c48e461b1b3af020aa21e98
BuGalter <wardoc78@gmail.com>
2022-01-26 11:34:52 +0300

    feat: Added to routes reg and auth auth: false

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -16,11 +16,17 @@ export default [
   {
     method: 'POST',
     path: '/user/reg',
+    options: {
+      auth: false,
+    },
     handler: userReg,
   },
   {
     method: 'POST',
     path: '/user/auth',
+    options: {
+      auth: false,
+    },
     handler: userAuth,
   }
 ];
