fe5863f2bcfe4062a804b6a4c498fe6df670679a
BuGalter <wardoc78@gmail.com>
2022-01-29 16:51:53 +0300

    feat: Added handlers for routes
    Added handler for route profile/student/reg
    Added handler for route profile/teacher/reg
    Added function isUser

---
+++ src/server/api/profile.ts
@@ -0,0 +1,47 @@
+import { Student, } from '../models/Student';
+import { Teacher, } from '../models/Teacher';
+import { User, } from '../models/User';
+import { output, error, } from '../utils';
+
+async function isUser(userId) {
+  const user = await User.findByPk(userId);
+  if (!user) {
+    return false;
+  }
+
+  return true;
+}
+
+export async function studentReg(r) {
+  const { userId, faculty, universityId, group, } = r.payload;
+  if (!isUser(userId)) {
+    return error(404000, `User not found!`, null);
+  }
+
+  const student = await Student.findOne({
+    where: { userId, universityId, },
+  });
+  if (!student) {
+    const newStudent = await Student.create({ userId, faculty, universityId, group, });
+    return output({ message: `Student ${newStudent.id} added!`, });
+  }
+
+  return error(400000, `Student already exists!`, null);
+}
+
+export async function teacherReg(r) {
+  const { userId, faculty, universityId, } = r.payload;
+  if (!isUser(userId)) {
+    return error(404000, `User not found!`, null);
+  }
+
+  const teacher = await Teacher.findOne({
+    where: { userId, universityId, },
+  });
+  if (!teacher) {
+    const newTeacher = await Teacher.create({ userId, faculty, universityId, group, });
+    return output({ message: `Student ${newTeacher.id} added!`, });
+  }
+
+  return error(400000, `Teacher already exists!`, null);
+}
