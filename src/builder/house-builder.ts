d2c961b7d4a3b06c409a282b9648286df888b734
BuGalter <wardoc78@gmail.com>
2022-01-31 20:38:26 +0300

    feat: Added pattern builder task

---
+++ src/builder/house-builder.ts
@@ -0,0 +1,62 @@
+interface Builder {
+  addDitch(): void;
+  addBasement(): void;
+  addWalls(): void;
+  addFacade(): void;
+  addRoff(): void;
+}
+
+class House {
+  public partsHouse: string[] = [];
+
+  public getHouseParts(): void {
+    console.log(`Дом состоит из: ${this.partsHouse.join(', ')}`);
+  }
+}
+
+class HouseBuilder implements Builder {
+  private house: House;
+
+  constructor() {
+    this.init();
+  }
+
+  public init(): void {
+    this.house = new House();
+  }
+
+  public addDitch(): void {
+    this.house.partsHouse.push('Катлован');
+  }
+
+  public addBasement() {
+    this.house.partsHouse.push('Фундамент');
+  }
+
+  public addWalls() {
+    this.house.partsHouse.push('Стены');
+  } 
+
+  public addFacade() {
+    this.house.partsHouse.push('Фасад');
+  }
+
+  public addRoff() {
+    this.house.partsHouse.push('Крыша');
+  }
+
+  public buildHouse(): House {
+    const structure = this.house;
+    this.init();
+    return structure;
+  }
+}
+
+const house = new HouseBuilder();
+house.addDitch();
+house.addBasement();
+house.addWalls();
+house.addFacade();
+house.addRoff();
+house.buildHouse().getHouseParts();
+
