7b02dc6d45fbdbfdec9f0e87ecb54a5f074b00c2
BuGalter <wardoc78@gmail.com>
2022-02-06 16:21:35 +0300

    feat: Added to test test student and teacher

---
+++ test/test-data.ts
@@ -0,0 +1,30 @@
+
+export const testStudent = {
+  name: 'testStudent',
+  email: 'testStudent@test.com',
+  password: 'testS',
+  phone: '+79004445566',
+  birth: new Date(1995, 11, 17),
+  sex: 'female',
+  userId: '',
+  faculty: 'IBM',
+  universityId: 'LETI',
+  group: 7700,
+  token: '',
+  studentId: ''
+};
+
+export const testTeacher = {
+  name: 'testTeacher',
+  email: 'testTeacher@test.com',
+  password: 'testT',
+  phone: '+79004447788',
+  birth: new Date(1979, 3, 12),
+  sex: 'male',
+  userId: '',
+  faculty: 'IBM',
+  universityId: 'LETI',
+  token: '',
+  teacherId: ''
+};
+
